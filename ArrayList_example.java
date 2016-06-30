069d3b330f3f3ab3ef289dab871c252600a35b20
Raounak Sharma <raounaqsharma@gmail.com>
2016-06-30 19:48:03 +0530

    one more file added

---
+++ ArrayList_example.java
@@ -0,0 +1,24 @@
+package ronaq;
+
+import java.util.ArrayList;
+
+public class learn {
+
+	public static void main(String args[]) throws Exception
+	{
+		 ArrayList l =new ArrayList();
+		 l.add("A");
+		 l.add(10);
+		 l.add("A");
+		 l.add(null);
+		 System.out.println(l);
+		 l.remove(2);
+		 System.out.println(l);
+		 l.add(2, "M");
+		 l.add("N");
+		 System.out.println(l);
+		 	
+	}
+}
+
+
