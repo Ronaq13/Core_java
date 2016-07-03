b7d4424d90c4f082fd792e7769c15f692d0463d5
Raounak Sharma <raounaqsharma@gmail.com>
2016-07-03 12:08:38 +0530

    modified

--- hashSet_example.java
+++ hashSet_example.java
@@ -8,8 +8,8 @@ public class hashSet_example
         h.add("C");
         h.add("D");
         h.add("Z");
-        h.add("null");
-        h.add("10");  
+        h.add(null);
+        h.add(10);  
         System.out.println(h.add("Z"));    // duplicates are not allowed hence gives false
         System.out.println(h);
     }
