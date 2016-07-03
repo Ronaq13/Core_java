6d9c9c043be2558f690dbc9c88edef20a93f16dc
Raounak Sharma <raounaqsharma@gmail.com>
2016-07-03 11:53:16 +0530

    Collection Framework programs

---
+++ hashSet_example.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class hashSet_example
+{
+    public static void main(String args[])
+    {
+        HashSet h = new HashSet();
+        h.add("B");
+        h.add("C");
+        h.add("D");
+        h.add("Z");
+        h.add("null");
+        h.add("10");  
+        System.out.println(h.add("Z"));    // duplicates are not allowed hence gives false
+        System.out.println(h);
+    }
+}
